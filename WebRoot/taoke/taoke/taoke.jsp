105648a9802e835a51653b7b8d16992e17326b6c
duan <1021567205@qq.com>
2017-06-14 11:41:29 +0800

    测试套壳

--- WebRoot/taoke/taoke/taoke.jsp
+++ WebRoot/taoke/taoke/taoke.jsp
@@ -23,6 +23,6 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
   </head>
   
   <body>
-  1
+  <%=request.getAttribute("html") %>
   </body>
 </html>
--- src/com/clint/taokewangzhan/controller/TaoKeController.java
+++ src/com/clint/taokewangzhan/controller/TaoKeController.java
@@ -1,5 +1,13 @@
 package com.clint.taokewangzhan.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +16,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/taoke")
 public class TaoKeController {
 
+	
+	@SuppressWarnings("unused")
 	@RequestMapping(value = "/toTaoke")
-	public String toTaoke(){
-		System.out.println("1111111111111111");
+	public String toTaoke(HttpServletRequest request){
+		
+		String url = "http://m.mmjpg.com/";
+		
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Elements elements = doc.getElementsByTag("img");
+		for (int i = 0; i < elements.size(); i++) {
+			Element element = elements.get(i);
+			System.out.println(element.attr("data-img"));
+		}
+		
+		System.out.println(doc.toString());
+		if (doc==null) {
+			request.setAttribute("html", "访问失败");
+		}else{
+			request.setAttribute("html", doc.toString());
+		}
+		
 		return "taoke/taoke.jsp";
 	}
 }
