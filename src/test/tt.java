cecc990849b806a8dd2a7e440db1dca21921fc16
duan <1021567205@qq.com>
2017-08-21 16:07:11 +0800

    1

---
+++ src/test/tt.java
@@ -0,0 +1,23 @@
+package test;
+
+import com.qiniu.util.Auth;
+
+public class tt {
+	// 设置好账号的ACCESS_KEY和SECRET_KEY
+	String ACCESS_KEY = "eiG_hSVKio6Yu7ouYmUDWoOAdSiZreYTJwkUL0K4";
+	String SECRET_KEY = "6j8-RmG6JS9NYibZFwzsdufvvQVn9GLNhFVby8ND";
+	// 要上传的空间
+	String bucketname = "keke";
+
+	// 密钥配置
+	Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+
+	// 简单上传，使用默认策略，只需要设置上传的空间名就可以了
+	public String getUpToken() {
+		return auth.uploadToken(bucketname);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new tt().getUpToken());;
+	}
+}
